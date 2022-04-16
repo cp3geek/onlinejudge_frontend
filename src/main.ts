35a0796bc0af51da5db77cf0bd00c3c64dfbec04
cp3 <792716300@qq.com>
2022-04-16 15:14:34 +0800

    build(main.ts): 引入element-ui

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import ElementPlus from 'element-plus'
 import router from '@/router/index'
 import store from '@/store/index'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App).use(router).use(store).use(ElementPlus).mount('#app')
