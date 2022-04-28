3c5b9767b478d0583a3996997ec9769d77b4f671
cp3 <792716300@qq.com>
2022-04-28 16:26:31 +0800

    refactor(views): 去除登陆请求，准备搭建OJ首页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,15 +4,10 @@
 
 
 <script lang="ts">
-import {userlogin}from "@/api/index"
-import { onMounted } from 'vue-demi'
+
 
 export default{
-    setup(){
-        onMounted(()=>{
-            userlogin("胡昌江","123456")
-        })
-    }
+    
 }
 
 </script>
