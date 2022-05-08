e62701f43074fb072b90d2d13ad0eb6960401bf3
cp3 <792716300@qq.com>
2022-05-08 14:57:12 +0800

    test(main.ts): vue3学习

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,5 @@ import ElementPlus from 'element-plus'
 import router from '@/router/index'
 import store from '@/store/index'
 import 'element-plus/dist/index.css'
-
+//程序的主入口文件
 createApp(App).use(router).use(store).use(ElementPlus).mount('#app')
