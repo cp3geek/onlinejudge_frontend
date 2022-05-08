dbbe52cea64765c67e7841667328d466f680e464
cp3 <792716300@qq.com>
2022-05-08 15:40:29 +0800

    perf(package.json): 移除element-plus依赖

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+//程序的主入口文件,ts文件
+//引入createApp函数，创建对应的应用，产生应用的实例对象
 import { createApp } from 'vue'
+//引入App组件（所有组件的父级组件）
 import App from './App.vue'
-import ElementPlus from 'element-plus'
+//引入其他组件
 import router from '@/router/index'
 import store from '@/store/index'
 import 'element-plus/dist/index.css'
-//程序的主入口文件
-createApp(App).use(router).use(store).use(ElementPlus).mount('#app')
+//创建App应用返回对应的实例对象，调用mount方法进行挂载
+createApp(App).use(router).use(store).mount('#app')
