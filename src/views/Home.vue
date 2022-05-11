6f2ec2b4fff16331d8050e664cec1dd307977eea
cp3 <792716300@qq.com>
2022-05-11 10:00:51 +0800

    feat(home.vue): 设计页面主内容部分

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,7 +24,25 @@
         </n-layout>
       </n-layout-header>
       <n-layout-content content-style="padding: 24px;">
-        核心部分
+        <n-layout embedded content-style="padding: 24px;">
+          <n-card>
+            <n-grid cols="4" item-responsive>
+              <n-grid-item span="0 400:1 600:2 800:3">
+                <div class="light-green">
+                  0～400px：不显示<br>
+                  400～600px：占据空间 1<br>
+                  600～800px：占据空间 2<br>
+                  800px 以上：占据空间 3
+                </div>
+              </n-grid-item>
+              <n-grid-item>
+                <div class="green">
+                  2
+                </div>
+              </n-grid-item>
+            </n-grid>
+          </n-card>
+        </n-layout>
       </n-layout-content>
       <n-layout-footer bordered>页尾</n-layout-footer>
     </n-layout>
@@ -49,5 +67,21 @@ export default {}
 .n-layout-content {
   background: rgba(128, 128, 128, 0.2);
   margin-bottom: 10px;
+}
+
+.light-green {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 200px;
+  background-color: rgba(0, 128, 0, 0.12);
+}
+
+.green {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 200px;
+  background-color: rgba(0, 128, 0, 0.24);
 }
 </style>
